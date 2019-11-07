955b4495fd4ff79077d89d2673d17a08a0b60380
Hannah Arnett <hannaharnett@Hannahs-MacBook-Air-2.local>
2019-11-07 08:37:17 -0800

    Add form validation
    Author:    Hannah Arnett <hannahtesaker@gmail.com>

--- src/CreditCardForm.js
+++ src/CreditCardForm.js
@@ -1,71 +1,112 @@
 import React, { Component } from "react";
 import VirtualCreditCard from "./VirtualCreditCard";
 
+// Regexes
+const numberRegex = /^\d{16}$/;
+const expRegex = /^\d{4}$/;
+const cvcRegex = /^\d{3}$/;
+
+// If error object in state is empty, form is valid
+const formValidation = errors => {
+  let valid = true;
+  Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+  return valid;
+};
+
 export default class CreditCardForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
+      cardholder: "",
       number: "",
       exp: "",
-      cvc: ""
+      cvc: "",
+      formValidation: false,
+      errors: {
+        cardholder: "",
+        number: "",
+        exp: "",
+        cvc: ""
+      }
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  // handles changes to all inputs in form
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-  // resets state to empty
+  // handles changes to inputs and error object
+  handleChange = event => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case "cardholder":
+        errors.cardholder =
+          value.length < 1 ? "Please enter Cardholder's name." : "";
+        break;
+      case "number":
+        errors.number = numberRegex.test(value)
+          ? ""
+          : "Card number is not valid.";
+        break;
+      case "exp":
+        errors.exp = expRegex.test(value) ? "" : "Date is not correct.";
+        break;
+      case "cvc":
+        errors.cvc = cvcRegex.test(value) ? "" : "Security code is not valid.";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+  };
+  // checks validity on submit and resets state
   handleSubmit(e) {
     e.preventDefault();
-    this.setState({ name: "", number: "", exp: "", cvc: "" });
+    this.setState({ formValidation: formValidation(this.state.errors) });
+    this.setState({ cardholder: "", number: "", exp: "", cvc: "" });
   }
   render() {
+    const { cardholder, number, exp, cvc, formValidation } = this.state;
     return (
       <div>
         <VirtualCreditCard
-          name={this.state.name}
-          number={this.state.number}
-          exp={this.state.exp}
-          cvc={this.state.cvc}
+          cardholder={cardholder}
+          number={number}
+          exp={exp}
+          cvc={cvc}
         />
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
-            name="name"
-            value={this.state.name}
-            placeholder="NAME ON CARD"
+            name="cardholder"
+            value={cardholder}
+            placeholder="NAME"
             onChange={this.handleChange}
-            required
           />
           <input
             type="text"
             name="number"
-            value={this.state.number}
+            value={number}
             placeholder="CARD NUMBER"
-            maxLength="16"
             onChange={this.handleChange}
-            required
           />
           <input
             type="text"
             name="exp"
-            value={this.state.exp}
+            value={exp}
             placeholder="GOOD THROUGH"
             onChange={this.handleChange}
-            required
           />
           <input
             type="text"
             name="cvc"
-            value={this.state.cvc}
+            value={cvc}
             placeholder="SECURITY CODE"
             onChange={this.handleChange}
-            required
           />
           <input type="submit" value="Submit" />
+          <p>Form is {formValidation ? "submitted" : "invalid"}</p>
         </form>
       </div>
     );
--- src/VirtualCreditCard.js
+++ src/VirtualCreditCard.js
@@ -1,13 +1,6 @@
 import React from "react";
 import "./credit-card.css";
 
-//const cards = {
-//    3: "Travel/entertainment cards (e.g. American Express and Diners Club)",
-//4: "Visa",
-//5: "MasterCard",
-//6: "Discover Card"
-//}
-
 export default props => {
   return (
     <div className="credit-card">
@@ -15,8 +8,8 @@ export default props => {
       <div className="credit-card-number">{props.number}</div>
       <div className="credit-card-info">
         <div className="credit-card-info-name">
-          <div className="credit-card-info-name-label">NAME HERE</div>
-          <div>{props.name}</div>
+          <div className="credit-card-info-name-label">CARDHOLDER'S NAME</div>
+          <div>{props.cardholder}</div>
         </div>
         <div className="credit-card-info-expiry">
           <div className="credit-card-info-exiry-label">VALID UNTIL</div>

17a18b5bd7cad4680513ffb8afbabfe93f8d3eb6
Hannah Arnett <hannaharnett@Hannahs-MacBook-Air-2.local>
2019-11-07 15:51:05 -0800

    Add automatic logo detection

--- src/CreditCardForm.js
+++ src/CreditCardForm.js
@@ -6,11 +6,69 @@ const numberRegex = /^\d{16}$/;
 const expRegex = /^\d{4}$/;
 const cvcRegex = /^\d{3}$/;
 
+const defaultProps = {
+  cards: [
+    {
+      name: "blank",
+      src:
+        "https://upload.wikimedia.org/wikipedia/commons/d/df/Map-circle-grey.svg"
+    },
+    {
+      name: "mastercard",
+      src:
+        "https://upload.wikimedia.org/wikipedia/commons/0/04/Mastercard-logo.png"
+    },
+    {
+      name: "visa",
+      src:
+        "https://upload.wikimedia.org/wikipedia/commons/5/53/Visa_2014_logo_detail.svg"
+    },
+    {
+      name: "dinersclub",
+      src:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a6/Diners_Club_Logo3.svg"
+    },
+    {
+      name: "americanExpress",
+      src:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/American_Express_logo.svg/600px-American_Express_logo.svg.png"
+    }
+  ]
+};
+
 // If error object in state is empty, form is valid
 const formValidation = errors => {
   let valid = true;
   Object.values(errors).forEach(val => val.length > 0 && (valid = false));
   return valid;
+};
+
+// Checks the first digits in number input and sets the right logo src attribute
+const findLogo = number => {
+  let cards = defaultProps.cards;
+  let src;
+  if (
+    parseInt(number.substring(0, 2)) > 50 &&
+    parseInt(number.substring(0, 2)) < 56
+  ) {
+    src = cards[1].src;
+  } else if (parseInt(number.substring(0, 1)) === 4) {
+    src = cards[2].src;
+  } else if (
+    parseInt(number.substring(0, 2)) === 36 ||
+    parseInt(number.substring(0, 2)) === 38 ||
+    parseInt(number.substring(0, 2)) === 39
+  ) {
+    src = cards[3].src;
+  } else if (
+    parseInt(number.substring(0, 2)) === 34 ||
+    parseInt(number.substring(0, 2)) === 37
+  ) {
+    src = cards[4].src;
+  } else {
+    src = cards[0].src;
+  }
+  return src;
 };
 
 export default class CreditCardForm extends Component {
@@ -21,6 +79,8 @@ export default class CreditCardForm extends Component {
       number: "",
       exp: "",
       cvc: "",
+      logoSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/d/df/Map-circle-grey.svg",
       formValidation: false,
       errors: {
         cardholder: "",
@@ -36,7 +96,7 @@ export default class CreditCardForm extends Component {
   handleChange = event => {
     event.preventDefault();
     const { name, value } = event.target;
-    let errors = this.state.errors;
+    let { errors, number } = this.state;
 
     switch (name) {
       case "cardholder":
@@ -59,6 +119,7 @@ export default class CreditCardForm extends Component {
     }
 
     this.setState({ errors, [name]: value });
+    this.setState({ logoSrc: findLogo(number) });
   };
   // checks validity on submit and resets state
   handleSubmit(e) {
@@ -67,7 +128,14 @@ export default class CreditCardForm extends Component {
     this.setState({ cardholder: "", number: "", exp: "", cvc: "" });
   }
   render() {
-    const { cardholder, number, exp, cvc, formValidation } = this.state;
+    const {
+      cardholder,
+      number,
+      exp,
+      cvc,
+      formValidation,
+      logoSrc
+    } = this.state;
     return (
       <div>
         <VirtualCreditCard
@@ -75,6 +143,7 @@ export default class CreditCardForm extends Component {
           number={number}
           exp={exp}
           cvc={cvc}
+          logoSrc={logoSrc}
         />
         <form onSubmit={this.handleSubmit}>
           <input
